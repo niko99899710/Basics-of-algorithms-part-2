93fabda71cb112b79f98505b2e46e72be6b2ae6b
Karina <bagdasarova-k@inbox.ru>
2023-07-17 04:17:12 +0500

    Basics of algorithms part 2

---
+++ src/main/java/com/example/basicsofalgorithmspart2/BasicsOfAlgorithmsPart2Application.java
@@ -0,0 +1,13 @@
+package com.example.basicsofalgorithmspart2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicsOfAlgorithmsPart2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicsOfAlgorithmsPart2Application.class, args);
+    }
+
+}
